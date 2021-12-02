5b2c830796f5b388e22a948d4a3d761270fcc673
Larissa Porto <larissaporto@live.com>
2021-12-02 01:12:41 -0300

    Add play_track function and edit main accordingly

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os, requests, json, logging, time
 
 logging.basicConfig()  
 logger=logging.getLogger()
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.ERROR)
 
 SP_DC = os.environ['SP_DC']
 
@@ -47,10 +47,48 @@ def get_friend_activity(friends_activity: list, friend_name: str) -> dict or Non
     logger.error(f"No user {friend_name} found on friends acitivity.")
 
 
+def play_track(user_id: str, track_id: str) -> None:
+    import spotipy
+    import spotipy.util as util
+    from json.decoder import JSONDecodeError
+
+    scope = 'user-read-private user-read-playback-state user-modify-playback-state'
+
+    # Erase cache and prompt for user permission
+    try:
+        token = util.prompt_for_user_token(user_id, scope) # add scope
+    except (AttributeError, JSONDecodeError):
+        os.remove(f".cache-{user_id}")
+        token = util.prompt_for_user_token(user_id, scope) # add scope
+
+    # Create our spotify object with permissions
+    spotifyObject = spotipy.Spotify(auth=token)
+
+    # Get current device
+    try:
+        devices = spotifyObject.devices()
+        deviceID = devices['devices'][0]['id']
+    except IndexError:
+        print("No connected devices.")
+
+    # Play Song
+    spotifyObject.start_playback(deviceID, None, [track_id])
+    # print(spotifyObject.current_user()['id'])
+
+
 def main():
+    # Provide your used_id and friends name
+    USER_ID = ''
+    FRIEND_NAME = ''
+
     web_access_token = get_access_token(SP_DC)
     friends_activity = get_all_friends_activity_list(web_access_token)
-    print(get_friend_activity(friends_activity, "Vicente"))
+
+    friend_activity = get_friend_activity(friends_activity, FRIEND_NAME)
+    print(friend_activity)
+    track_uri = friend_activity['uri']
+
+    play_track(USER_ID, track_uri)
 
 
 if __name__ == "__main__":
